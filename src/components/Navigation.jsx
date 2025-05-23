2cac8cdb3f6b3f0b33e5f0f3e1e1ab050912c060
marrriiooo <mariosenawati@gmail.com>
2025-05-24 06:27:11 +0700

    meperbarui aplikas

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
-import { useTheme } from "../contexts/ThemeContext"; // Pastikan path sesuai
+import { useTheme } from "../contexts/ThemeContext";
 
 const Navigation = ({ name, onLogout }) => {
   const { theme, toggleTheme } = useTheme();
